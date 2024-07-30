a2ecbd3fbc36bd6486e7a054c013d6762d042c57
marcus <marcusyoung@Marcuss-MacBook-Pro-3.local>
2024-07-29 21:24:41 -0700

    skeleton ui with s3 + dynamodb writes

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <LandingPage />
+  </div>
+</template>
+
+<script>
+import LandingPage from './components/LandingPage.vue';
+
+export default {
+  name: 'App',
+  components: {
+    LandingPage
+  }
+};
+</script>
+
+<style>
+/* Add your global styles here */
+</style>
