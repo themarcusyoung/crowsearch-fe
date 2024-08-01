2794360fe60904e50a9e35330dbbe9f0c43bdf2a
Marcus Young <marcusgyoung.business@gmail.com>
2024-08-01 09:57:37 -0700

    lingering changes

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
     <CrowSearch />
-=======
-    <LandingPage />
->>>>>>> a2ecbd3fbc36bd6486e7a054c013d6762d042c57
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
 import CrowSearch from './components/CrowSearch.vue';
-=======
-import LandingPage from './components/LandingPage.vue';
->>>>>>> a2ecbd3fbc36bd6486e7a054c013d6762d042c57
 
 export default {
   name: 'App',
   components: {
-<<<<<<< HEAD
     CrowSearch
-=======
-    LandingPage
->>>>>>> a2ecbd3fbc36bd6486e7a054c013d6762d042c57
   }
 };
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,5 @@
 module.exports = {
-<<<<<<< HEAD
   publicPath: process.env.NODE_ENV === 'production'
     ? '/crowsearch-fe/'
     : '/'
 }
-=======
-  transpileDependencies: [],
-};
->>>>>>> a2ecbd3fbc36bd6486e7a054c013d6762d042c57
